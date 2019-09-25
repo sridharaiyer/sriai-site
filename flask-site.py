beaf8f0bbc1caa7255124afe697aae425fd53565
Sridhar Iyer <siyer@cccis.com>
2019-09-25 15:35:15 -0500

    IMDB work in progress

--- flask-site.py
+++ flask-site.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm, IMDBForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -23,7 +23,7 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html', posts=posts)
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
@@ -50,6 +50,15 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/imdb", methods=['GET', 'POST'])
+def imdb():
+    form = IMDBForm()
+    if form.validate_on_submit():
+        flash('Positive Review!', 'message')
+
+    return render_template('imdb.html', title='IMDB User Review Sentiment Analysis', form=form)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -28,3 +28,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+
+class IMDBForm(FlaskForm):
+    """docstring for IMDBForm."""
+
+    user_review = TextAreaField('User Review', validators=[
+                                DataRequired()], render_kw={"rows": 3, "cols": 11})
+    submit = SubmitField('Submit')
